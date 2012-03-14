ffb0cb9e3e1e065107db65296cfc6d106728de0a
Donatas Martinkus <donatasmart@gmail.com>
2012-03-14 21:59:47 +0000

    initial commit: xFrame create-project product + adding libraries

---
+++ src/homepage/controller/Index.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace homepage\controller;
+use \xframe\request\Controller;
+
+/**
+ * Controller for the demo 
+ *
+ * @author Linus Norton <linusnorton@gmail.com>
+ */
+class Index extends Controller {
+
+    /**
+     * @Request("index")
+     */
+    public function run() {
+
+    }
+
+}
+
