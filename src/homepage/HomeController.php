b80158296647307f72da13be03ae58264aef09b7
Donatas Martinkus <donatasmart@gmail.com>
2012-03-20 17:37:53 +0000

    added google-analytics code, google site-verification meta tag and meta description

--- src/homepage/HomeController.php
+++ src/homepage/HomeController.php
@@ -9,5 +9,6 @@ class HomeController extends Controller {
     public function init() {
         $html = new \HtmlInit($this->dic->registry);
         $this->view->html = $html->getDefaults();
+        $this->view->showGA = CONFIG == "live" ? 1 : 0;
     }
 }
